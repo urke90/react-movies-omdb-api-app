0f7a409d37c5b44e2c6c374f777bbc0132cd5368
urke90 <urosbijelic90@gmail.com>
2020-12-15 21:16:57 +0100

    created movieList component and added dummy data in App.js

---
+++ src/components/MovieList.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const MovieList = ({ movies }) => {
+  return (
+    <>
+      {movies.map((movie, index) => (
+        <div className="image-container d-flex justify-content-start m-3">
+          <img src={movie.Poster} alt="movie"></img>
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default MovieList;
