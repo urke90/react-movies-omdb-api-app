ee29973aee9c3cbab8ae0824187f106065626fe0
urke90 <urosbijelic90@gmail.com>
2020-12-15 21:42:59 +0100

    added search component and header

--- src/App.js
+++ src/App.js
@@ -1,38 +1,33 @@
 import React, { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import axios from "axios";
+import MovieList from "./components/MovieList";
+import MovieListHeading from "./components/MovieListHeading";
+import Search from "./components/Search";
 
 const App = () => {
-  const [movies, setMovies] = useState([
-    {
-      Title: "Star Wars: Episode IV - A New Hope",
-      Year: "1977",
-      imdbID: "tt0076759",
-      Type: "movie",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BNzVlY2MwMjktM2E4OS00Y2Y3LWE3ZjctYzhkZGM3YzA1ZWM2XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg",
-    },
-    {
-      Title: "Star Wars: Episode V - The Empire Strikes Back",
-      Year: "1980",
-      imdbID: "tt0080684",
-      Type: "movie",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BYmU1NDRjNDgtMzhiMi00NjZmLTg5NGItZDNiZjU5NTU4OTE0XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg",
-    },
-    {
-      Title: "Star Wars: Episode VI - Return of the Jedi",
-      Year: "1983",
-      imdbID: "tt0086190",
-      Type: "movie",
-      Poster:
-        "https://m.media-amazon.com/images/M/MV5BOWZlMjFiYzgtMTUzNC00Y2IzLTk1NTMtZmNhMTczNTk0ODk1XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg",
-    },
-  ]);
+  const [searchValue, setSearchValue] = useState("");
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=263d22d8`;
+      const response = await axios.get(url);
+      console.log(response.data);
+      setMovies(response.data.search);
+    };
+
+    fetchMovies();
+  }, [searchValue]);
 
   return (
     <div className="container-fluid movie-app">
-      <div className="row"></div>
+      <div className="row d-flex align-items-center mt-4 mb-4">
+        <MovieListHeading heading="OMBD Movies!" />
+        <Search value={searchValue} setSearchValue={setSearchValue} />
+      </div>
+      <div className="row">{movies && <MovieList movies={movies} />}</div>
     </div>
   );
 };
